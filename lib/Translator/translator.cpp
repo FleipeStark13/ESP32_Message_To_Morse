926e9071455a6e2b002dbf7183afc3beec91aac7
FleipeStark13 <felipe.liandrojesus13@gmail.com>
2026-04-06 21:29:51 -0300

    fix: add the .cpp files for headers.

---
+++ lib/Translator/translator.cpp
@@ -0,0 +1,72 @@
+#include "translator.h"
+
+const char* MORSE_ALPHABET[] = {
+    ".-",       // A
+    "-...",     // B
+    "-.-.",     // C
+    "-..",      // D
+    ".",        // E
+    "..-.",     // F
+    "--.",      // G
+    "....",     // H
+    "..",       // I
+    ".---",     // J
+    "-.-",      // K
+    ".-..",     // L
+    "--",       // M
+    "-.",       // N
+    "---",      // O
+    ".--.",     // P
+    "--.-",     // Q
+    ".-.",      // R
+    "...",      // S
+    "-",        // T
+    "--.",      // U
+    "...-",     // V
+    ".--",      // W
+    "-..-",     // X
+    "-.--",     // Y
+    "--..",      // ZZ
+};
+
+void sendDash() {
+    tone(WIRE, 1200);
+    delay(DASH_DELAY);
+    noTone(WIRE);
+    delay(CHAR_SPACE);
+}
+
+void sendDot() {
+    tone(WIRE, 2500);
+    delay(DOT_DELAY);
+    noTone(WIRE);
+    delay(CHAR_SPACE);
+}
+
+void processChar(char c) {
+    c = toupper(c);
+
+    if (c >= 'A' && c <= 'Z') {
+        int index = c - 'A';
+        const char* morseCode = MORSE_ALPHABET[index];
+
+        for (int i = 0; morseCode[i] != '\0'; i++) {
+            if (morseCode[i] == '.') {
+                sendDot();
+            } else if (morseCode[i] == '-') {
+                sendDash();
+            }
+            delay(CHAR_SPACE);
+        }
+    } else if (c == ' ') {
+        delay(SPACE_TIME);
+    } 
+}
+
+void translator(const char* word) {
+    for (int i = 0; word[i] != '\0'; i++) {
+        processChar(word[i]);
+    }
+}
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <Wire.h>
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
-#include <translator.h>
-#include <config.h>
+#include "translator.h"
+#include "config.h"
 
 LiquidCrystal_I2C lcd(0x27, LCD_COLUMNS, LCD_ROWS);
 void drawLcdLetter(char* letter) {
